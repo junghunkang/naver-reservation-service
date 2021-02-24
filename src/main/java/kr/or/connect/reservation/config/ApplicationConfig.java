47a765f21156f91fb387ea1e062aaf7c7e1fefe5
junghunkang <gns0187@gmail.com>
2021-02-24 17:36:11 +0900

    add spring mvc configuration

---
+++ src/main/java/kr/or/connect/reservation/config/ApplicationConfig.java
@@ -0,0 +1,12 @@
+package kr.or.connect.reservation.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
+
+@Configuration
+@ComponentScan(basePackages = {"kr.or.connect.dao","kr.or.connect.service"})
+@Import({DBConfig.class})
+public class ApplicationConfig {
+
+}
